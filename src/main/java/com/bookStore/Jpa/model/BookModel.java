51baa566cf31df94a1b87bc0bd08e43e920de57e
Thiago <thiagofonsecaclaudino@gmail.com>
2024-08-31 01:10:50 -0300

    feat: adicionando relacionamento manyToOne  entre o book e publisher

--- src/main/java/com/bookStore/Jpa/model/BookModel.java
+++ src/main/java/com/bookStore/Jpa/model/BookModel.java
@@ -1,4 +1,50 @@
 package com.bookStore.Jpa.model;
 
-public class BookModel {
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_BOOK")
+public class BookModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+
+    //@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @ManyToOne //fetch = FetchType.LAZY)
+    @JoinColumn(name = "publisher_id")
+    private PublisherModel publisher;
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public PublisherModel getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(PublisherModel publisher) {
+        this.publisher = publisher;
+    }
 }
--- src/main/java/com/bookStore/Jpa/model/ReviewModel.java
+++ src/main/java/com/bookStore/Jpa/model/ReviewModel.java
@@ -1,4 +1,36 @@
 package com.bookStore.Jpa.model;
 
-public class ReviewModel {
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_REVIEW")
+public class ReviewModel implements Serializable {
+    private static final long seriaVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String comment;
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
