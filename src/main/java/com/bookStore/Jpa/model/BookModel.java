de2c5b15a0ad7b19fd7d8c19267a8839efab6a43
Thiago <thiagofonsecaclaudino@gmail.com>
2024-08-31 01:42:48 -0300

    feat: adicioando relacionamento OneToOne entre book e review

--- src/main/java/com/bookStore/Jpa/model/BookModel.java
+++ src/main/java/com/bookStore/Jpa/model/BookModel.java
@@ -35,6 +35,8 @@ public class BookModel implements Serializable {
     private Set<AuthorModel> authors = new HashSet<>();
 
 
+    @OneToOne(mappedBy = "book", cascade = CascadeType.ALL)
+    private ReviewModel review;
 
 
     public UUID getId() {
@@ -67,5 +69,13 @@ public class BookModel implements Serializable {
 
     public void setAuthors(Set<AuthorModel> authors) {
         this.authors = authors;
+    }
+
+    public ReviewModel getReview() {
+        return review;
+    }
+
+    public void setReview(ReviewModel review) {
+        this.review = review;
     }
 }
